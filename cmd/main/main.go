f8484e52e463f53b1dfe5f78dbd3735f3ecb10b1
Hayoun01 <3aba9irablog@gmail.com>
2024-03-24 02:13:34 +0000

    Unnecessary code

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -7,14 +7,9 @@ import (
 	"github.com/Hayoun01/book_store_api/pkg/config"
 	"github.com/Hayoun01/book_store_api/pkg/routes"
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load("./pkg/config/.env")
-	if err != nil {
-		log.Fatal(err)
-	}
 	r := mux.NewRouter()
 	v1Router := r.PathPrefix("/v1").Subrouter()
 	routes.RegisterBookRoutes(v1Router)
